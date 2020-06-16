0816ffeabd74d5b5645de0ac9057f6a90e606d61
Karl Keiser <karl.keiser@gmx.ch>
2020-06-16 14:30:27 +0200

    fix a rare crash to a parameter that is wrong in certain situations

--- src/dderlodpi.erl
+++ src/dderlodpi.erl
@@ -859,7 +859,13 @@ number_to_binary(Else) -> ?Error("Tried to convert bad term to binary: ~p", [Els
 %%%% Dpi safe functions executed on dpi slave node
 
 dpi_conn_prepareStmt(#odpi_conn{node = Node, connection = Conn}, Sql) ->
-    dpi:safe(Node, fun() -> dpi:conn_prepareStmt(Conn, false, Sql, <<"">>) end).
+    Stmt = dpi:safe(Node, fun() -> dpi:conn_prepareStmt(Conn, false, Sql, <<"">>) end), % tentative statement with scrollable set to false
+    #{statementType := StatementType} = dpi:safe(Node, fun() -> dpi:stmt_getInfo(Stmt) end), % fin out whether it is a SELECT
+    case StatementType of 'DPI_STMT_TYPE_SELECT' -> % SELECT statement needs to be redone with scrollable set to true
+        dpi:safe(Node, fun() -> dpi:stmt_close(Stmt) end), % remove the "bad" statement
+        dpi:safe(Node, fun() -> dpi:conn_prepareStmt(Conn, true, Sql, <<"">>) end); % return the proper statement
+    _Else -> Stmt end.   % if it is not a SELECT statement then the original Stmt handle is fine  
+
 
 dpi_conn_commit(#odpi_conn{node = Node, connection = Conn}) ->
     dpi:safe(Node, fun() -> dpi:conn_commit(Conn) end).
